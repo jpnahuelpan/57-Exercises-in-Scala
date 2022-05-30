8f0536b54dc6052652086b7cdd568b7d06aeaafd
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2022-05-29 22:57:24 -0400

    ejercicio 2
    se corrigen el nombre del ejercicio 1 xd, y además se añade un ejercicio 2 para probar el ciclo while

---
+++ exercise2.scala
@@ -0,0 +1,22 @@
+import scala.io.StdIn.readLine
+
+/** Counting the Number of Characters.
+  *
+  * @author Juan Pablo Nahuelpán
+  *
+  * Program that prompts for an input string and displays
+  * output that shows the input string and the number of
+  * characters the string contains.
+  */
+@main def main: Unit = {
+  println("What is the input string?");
+  var name = readLine();
+  while (verify(name)){
+    println("Please enter a valid string");
+    name = readLine();
+  }
+  println(s"$name has ${name.length} characters.")
+}
+
+def verify(name: String): Boolean =
+  name.length == 0
