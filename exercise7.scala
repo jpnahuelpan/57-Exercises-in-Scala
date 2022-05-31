17e3fbc8450d8ed9ef8cb58948a438a2f5d0993e
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2022-05-31 19:46:00 -0400

    agregando ejercicio 7
    en este ejercicio se atajan exceptiones te tipo conversión string a decimal(float)

---
+++ exercise7.scala
@@ -0,0 +1,40 @@
+import scala.io.StdIn.readLine
+import scala.util.{Try,Success,Failure}
+
+/** Area of a Rectangular Room
+  *
+  * @author Juan Pablo Nahuelpán
+  *
+  * Program that calculates the area of a room. Prompt
+  * the user for the length and width of the room in feet. Then
+  * display the area in both square feet and square meters.
+  */
+@main def main: Unit = {
+  var lrf: Float = 0.0f;
+  var wrf: Float = 0.0f; 
+  println("What is the length of the room in feet?");
+  val lengthRoomFeet = readLine();
+  println("What is the width of the room in feet?");
+  val widthRoomFeet = readLine();
+  println(s"You entered dimensions of $lengthRoomFeet feet by $widthRoomFeet feet.");
+  // validate input
+  try {
+      lrf = lengthRoomFeet.toFloat
+      wrf = widthRoomFeet.toFloat
+      // conversion square feet to square meters.
+      val squareFeet = lrf * wrf;
+      val squareMeters = squareFeetToSquareMeters(squareFeet);
+      // display results
+      println("The area is \n" +
+        s"$squareFeet square feet \n" +
+        s"$squareMeters square meters. \n");
+  } catch {
+      case e: NumberFormatException => println("Invalid input. Please try again. \n\n")
+      main
+  }
+}
+
+def squareFeetToSquareMeters(squareFeet: Float): Float = {
+  val conversionFactor = 0.09290304f;
+  squareFeet * conversionFactor;
+}
