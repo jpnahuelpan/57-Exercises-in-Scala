b53b2f6e97be3f3b7d9b0222189df00192c70821
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2022-07-20 01:15:36 -0400

    agregando ejercicio 16
    probando ternary operator en scala

---
+++ exercise16.scala
@@ -0,0 +1,27 @@
+import scala.io.StdIn.readLine
+
+/** Legal Driving Age
+  *
+  * @author Juan Pablo Nahuelpán
+  *
+  * Pogram that asks the user for their age and compare
+  * it to the legal driving age of sixteen. If the user is sixteen or
+  * older, then the program should display “You are old enough
+  * to legally drive.” If the user is under sixteen, the program
+  * should display “You are not old enough to legally drive.”
+  */
+@main def main: Unit = {
+  var getValues = true
+  while (getValues) {
+    try {
+      println("What is your age?")
+      val age = readLine.toInt
+      val msg = if (age >= 16) "are" else "are not"
+      println(s"You ${msg} enough to legally drive.")
+      getValues = false
+    }
+    catch {
+      case e: NumberFormatException => println("Invalid input. Please try again. \n\n")
+    }
+  }
+}
